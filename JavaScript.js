fc9fb9bb3fdb12d0e40588c40fe6b37c94a17dfe
NiallGr <niallgrainger@gmail.com>
2021-01-12 12:11:15 -0800

    ScrollFunction

--- JavaScript.js
+++ JavaScript.js
@@ -1,19 +1,17 @@
 
-
 psnMore.addEventListener("click", function () {
     document.getElementById('PSNFeatures').style.display = "initial";
     document.getElementById('PSNDescription').style.display = "none";
     document.getElementById('psnMore').style.display = "none";
     document.getElementById('psnLess').style.display = "initial";
-
 });
 psnLess.addEventListener("click", function () {
     document.getElementById('PSNDescription').style.display = "initial";
     document.getElementById('PSNFeatures').style.display = "none";
     document.getElementById('psnMore').style.display = "initial";
     document.getElementById('psnLess').style.display = "none";
+    
 });
-
 
 xboxMore.addEventListener("click", function () {
     document.getElementById('xboxFeatures').style.display = "initial";
@@ -42,18 +40,9 @@ comparisonLess.addEventListener("click", function () {
     document.getElementById('comparisonbtn').style.display = "initial";
 })
 // Scrool
-ScrollReveal().reveal('.display-2', { interval: 40 });
-ScrollReveal().reveal('#border-right', { interval: 140 });
-ScrollReveal().reveal('#border-left', { interval: 140 });
-ScrollReveal().reveal('#border1', { interval: 200 });
-ScrollReveal().reveal('#border2', { interval: 200 });
-ScrollReveal().reveal('#psControler', { delay: 700});
-ScrollReveal().reveal('#xboxController', { delay: 700});
-ScrollReveal().reveal('.green', { delay: 1400, interval: 100 });
-ScrollReveal().reveal('.blue', { delay: 1400, interval: 100 });
-
-
-
-
-
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.display-2', { interval: 50 });
+ScrollReveal().reveal('.row', { interval: 70 });
+ScrollReveal().reveal('.green', { delay: 1300, interval: 100 });
+ScrollReveal().reveal('.blue', { delay: 1300, interval: 100 });
 
